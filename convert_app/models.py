677ec3377e1bbbea772ede21df3eecf976be9ec2
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-14 20:46:36 +0900

    Remove cloudinary from model.py

--- convert_app/models.py
+++ convert_app/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class GrayModel(models.Model):
-    image = CloudinaryField(upload_to="input/gray")
-    gray_image = CloudinaryField(default = 'output/gray/gray.jpg')
+    image = models.ImageField(upload_to="input/gray")
+    gray_image = models.ImageField(default = 'output/gray/gray.jpg')
 
 class FaceReadModel(models.Model):
-    image = CloudinaryField(upload_to="input/faceread")
-    faceread_image = CloudinaryField(default = 'output/faceread/faceread.jpg')
+    image = models.ImageField(upload_to="input/faceread")
+    faceread_image = models.ImageField(default = 'output/faceread/faceread.jpg')
 
 class AnimeModel(models.Model):
-    image = CloudinaryField(upload_to="input/anime")
-    anime_image = CloudinaryField(default = 'output/anime/anime.jpg')
+    image = models.ImageField(upload_to="input/anime")
+    anime_image = models.ImageField(default = 'output/anime/anime.jpg')
 
 class MosaicModel(models.Model):
-    image = CloudinaryField(upload_to="input/mosaic")
-    mosaic_image = CloudinaryField(default = 'output/mosaic/mosaic.jpg')
+    image = models.ImageField(upload_to="input/mosaic")
+    mosaic_image = models.ImageField(default = 'output/mosaic/mosaic.jpg')
 
 class FaceMosaicModel(models.Model):
-    image = CloudinaryField(upload_to="input/facemosaic")
-    facemosaic_image = CloudinaryField(default = 'output/facemosaic/facemosaic.jpg')
+    image = models.ImageField(upload_to="input/facemosaic")
+    facemosaic_image = models.ImageField(default = 'output/facemosaic/facemosaic.jpg')
