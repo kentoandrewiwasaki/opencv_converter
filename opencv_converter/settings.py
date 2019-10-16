5e97f756f59420a3046acda8306b0fa42196d09a
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-16 18:37:00 +0900

    Transfer of secret_key to settings.py

--- opencv_converter/settings.py
+++ opencv_converter/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5h!g_49lt6yw7ux&xv_2v23tj##3tjfub^6n#a(%^52!j3-irn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
